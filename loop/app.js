0b33acbb70ce2edc4a1dcc3e6729409bc5127200
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-03-28 13:02:33 +0500

    home worek starting

--- loop/app.js
+++ loop/app.js
@@ -1,14 +1,17 @@
 var text = `Salom bahor hayir qish mana keldi bahor kettibdi qish endi boshlanadi ish.`;
 var textArr = text.split(" ");
 for (var i = 0; i < textArr.length; i++) {
-  console.log(textArr[i], i + 1);
+  for (var j = i + 1; i < textArr.length; i++) {
+    console.log(textArr[i], j);
+  }
 }
-// for (var key of textArr) {
-//   console.log(key);
-// }
-// for (var key in textArr) {
-//   console.log(key);
-// }
+console.log(textArr[i], i + 1);
+for (var key of textArr) {
+  console.log(key);
+}
+for (var key in textArr) {
+  console.log(key);
+}
 
 // for (var i = 0; i < textArr.length; i++) {
 //   for (let j = i + 1; j < textArr.length; j++) {
