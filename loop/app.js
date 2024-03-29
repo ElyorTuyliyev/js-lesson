07eb56249bf7a85dc7744d2c5fc26973a87671ad
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-03-29 11:18:47 +0500

    js file add

--- loop/app.js
+++ loop/app.js
@@ -1,17 +1,21 @@
-var text = `Salom bahor hayir qish mana keldi bahor kettibdi qish endi boshlanadi ish.`;
+var text = `Salom bahor hayir qish mana keldi ish bahor kettibdi qish endi boshlanadi ish`;
 var textArr = text.split(" ");
+
 for (var i = 0; i < textArr.length; i++) {
-  for (var j = i + 1; i < textArr.length; i++) {
-    console.log(textArr[i], j);
+  for (var j = i + 1; j < textArr.length; j++) {
+    if (textArr[i] === textArr[j]) {
+      console.log(`${textArr[i]} 2`);
+    }
   }
 }
-console.log(textArr[i], i + 1);
-for (var key of textArr) {
-  console.log(key);
-}
-for (var key in textArr) {
-  console.log(key);
-}
+
+// console.log(textArr[i], i + 1);
+// for (var key of textArr) {
+//   console.log(key);
+// }
+// for (var key in textArr) {
+//   console.log(key);
+// }
 
 // for (var i = 0; i < textArr.length; i++) {
 //   for (let j = i + 1; j < textArr.length; j++) {
