5a6fc74ed59d0d030df70134b48ef3d2041a5acf
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-04-03 15:10:10 +0500

    js file change and js correct

--- data transformer/app.js
+++ data transformer/app.js
@@ -3,7 +3,8 @@ var dateNow = new Date();
 var year = dateNow.getFullYear();
 var month = dateNow.getMonth() + 1;
 var day = dateNow.getDate();
-var dateNowToreplaceYear = format.replace("YYYY", year);
-var dateNowToreplaceMonth = dateNowToreplaceYear.replace("MM", month);
-var dateNowToreplaceDay = dateNowToreplaceMonth.replace("DD", day);
-console.log(dateNowToreplaceDay);
+var dateNowToreplaceYear = format
+  .replace("YYYY", year)
+  .replace("MM", month)
+  .replace("DD", day);
+console.log(dateNowToreplaceYear);
